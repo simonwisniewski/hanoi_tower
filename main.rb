9dec30c2ab4d6944d0fe95045b6f8d3eafc43ceb
Szymon Wiśniewski <smnwsnwsk@gmail.com>
2024-07-19 12:47:51 +0200

    valid-input-check

--- main.rb
+++ main.rb
@@ -3,6 +3,6 @@
 require_relative './hanoi_tower'
 
 print('Enter the number of disks: ')
-number_of_disks = gets.chomp.to_i
+number_of_disks = Integer(gets.chomp) rescue false
 solver = HanoiTower.new(number_of_disks)
 solver.solve
